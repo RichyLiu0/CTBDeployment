3803e7de31fe32b7e14317af500a294fbfe54fb3
RichyLiu0 <liuweirui@outlook.com>
2017-10-17 15:44:19 +0800

    列表优化
    分页优化

--- Web/Controllers/ProjectController.cs
+++ Web/Controllers/ProjectController.cs
@@ -25,6 +25,36 @@ namespace Deployment.Web.Controllers
                        OrderBy = t => t.OrderBy(to => to.CreateTime)
                    };
 
+            var ProjectID = "ProjectID".ValueOfQuery("");
+            if (ProjectID != "")
+            {
+                request.AddFilter(t => t.ProjectID == ProjectID);
+                //request.AddFilter(" AND ProjectID =@ProjectID").P("ProjectID", ProjectID);
+            }
+
+            var ProjectName = "ProjectName".ValueOfQuery("");
+            if (ProjectName != "")
+            {
+                request.AddFilter(t => t.ProjectName == ProjectName);
+            }
+
+            var ProjectCode = "ProjectCode".ValueOfQuery("");
+            if (ProjectCode != "")
+            {
+                request.AddFilter(t => t.ProjectCode == ProjectCode);
+            }
+
+            var minCreateTime = "minCreateTime".ValueOfQuery("1990-01-01").TryDateTime().Value;
+            if (minCreateTime != "1990-01-01".TryDateTime().Value)
+            {
+                request.AddFilter(t => t.CreateTime >=minCreateTime);
+            }
+
+            var maxCreateTime = "maxCreateTime".ValueOfQuery("1990-01-01").TryDateTime().Value;
+            if (maxCreateTime != "1990-01-01".TryDateTime().Value)
+            {
+                request.AddFilter(t => t.CreateTime <= maxCreateTime);
+            }   
             var list = DeploymentDAO.UnitService.CTBDeployment.PageList<D_Project>(request);
             return View(list);
         }
---
+++ Web/Models/PageModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Deployment.Web.Models
+{
+    public class PageModel
+    {
+        public int TotalCount {get;set;}
+        public int PageSize { get; set; }
+        public int PageIndex { get; set; }
+    }
+}
